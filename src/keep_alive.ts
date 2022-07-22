49142bbb07d52fd00d38378a35ad7f6f4a46637b
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-22 14:18:50 +0800

    chore(keep alive): add response interface

--- src/keep_alive.ts
+++ src/keep_alive.ts
@@ -1,3 +1,5 @@
+import { res } from "./types";
+
 const unnamed = require("unnamed-js");
 
 const PORT = process.env.PORT || 8080;
@@ -5,10 +7,6 @@ const server = unnamed({ port: PORT });
 const { GET } = server;
 
 server.router(require("./routers"));
-
-interface res extends Response {
-  send: (str: string) => void;
-}
 
 GET("/", (request: Request, response: res) => {
   response.send("Hello world");
